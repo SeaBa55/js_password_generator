b06ed3b63b81833e399b70744050abca9ee6ce22
sarrazol <sebastianarrazola@gmail.com>
2020-06-28 21:00:29 -0400

    Added functionality to the checkboxes, so that when selected they pass different character arrays to genPassVals. Selecting more than one checkbox concatinates the sring of characters selected; however, if no selections are made, an alert is provided to the user and we exit the entire genPass function call (script.js lines 18-50).

--- Develop/script.js
+++ Develop/script.js
@@ -14,13 +14,47 @@ function slideVal(){
 // Generate random password
 function genPass(){
 
+  // Check the state of the criteria checkboxes
+  var includeLowr = document.getElementById("lower_case").checked;
+  var includeCaps = document.getElementById("caps").checked;
+  var includeNumb = document.getElementById("numbers").checked;
+  var includeSymb = document.getElementById("special_char").checked;
+
+  // Initialize/ Reset password working parameter
+  let genPassVals = "";
+
+  // If user checked "Lower Case", add lowercase characters to genPassVals 
+  if(includeLowr == true){
+    genPassVals += "abcdefghijklmnopqrstuvwxyz";
+  }
+  
+  // If user checked "Caps", add uppercase characters to genPassVals 
+  if(includeCaps == true){
+    genPassVals += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } 
+  
+  // If user checked "Numbers", add number characters to genPassVals 
+  if(includeNumb == true){
+    genPassVals += "1234567890";
+  }
+  
+  // If user checked "Special Characters", add special characters to genPassVals
+  if(includeSymb == true){
+    genPassVals += "!@#$%^&*()_+-=`~";
+  }
+  
+  // If user makes no selection then push alert, and stop running genPass function
+  if((includeLowr || includeCaps || includeNumb || includeSymb) == false){
+    alert("Please select atlease one charater type");
+    return;
+  }
+
   // Initialize/ Reset password working parameter
   let password ="";
-  let genPassVals = "abcdefghijklmnopqrstuvwxyz";
 
   // create for loop to choose password characters - used < instead of <= to not includ the 8 element which would yeild 9 items since array starts at 0. 
   for(var i = 0; i < complexity; i++){
-      password = password + genPassVals.charAt(Math.floor(Math.random() * Math.floor(genPassVals.length - 1)));        
+    password = password + genPassVals.charAt(Math.floor(Math.random() * Math.floor(genPassVals.length - 1)));        
   }
 
   // add password to textbox/display area
